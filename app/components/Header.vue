458e0804fbad9517ab8d466edbd06624a9a667b6
oeyoews <jyao4783@gmail.com>
2025-10-13 15:07:55 +0800

    perf: update text

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -4,7 +4,7 @@
 			<UIcon name="i-lucide-cpu" class="size-6 mr-3 text-cyan-400 rotating-icon"></UIcon>
 			<h1
 				class="neo-glow text-3xl md:text-4xl font-extrabold tracking-wide bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 via-blue-400 to-indigo-500">
-				数据引接功能面板
+				项目系统功能面板
 			</h1>
 			<UIcon name="i-lucide-cpu" class="size-6 ml-3 text-cyan-400"></UIcon>
 		</div>
